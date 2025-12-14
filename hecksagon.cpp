48467ee893b965f766f4d03026f92abba9f0c415
PixieX2 <elijah.sjogren@gmail.com>
2025-12-14 04:41:34 +0000

    Update hecksagon.cpp

--- hecksagon.cpp
+++ hecksagon.cpp
@@ -1,89 +1,74 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int main(int argc, char** argv) {
-    if(argc < 2){
+    if (argc < 2) {
         cout << "Usage: " << argv[0] << " file.h2\n";
         return 1;
     }
 
-    ifstream f(argv[1]);
-    if(!f.is_open()){
+    ifstream f(argv[1], ios::binary);
+    if (!f) {
         cerr << "Cannot open file.\n";
         return 1;
     }
 
-    string code((istreambuf_iterator<char>(f)), istreambuf_iterator<char>());
+    string code((istreambuf_iterator<char>(f)), {});
     
-    int cell = 0;
-    int nose = 0;
-    int shadow = 0;
-    int ghost = 0; // extra hidden register
+    int cell = 0, nose = 0, shadow = 0, ghost = 0;
+    bool debug = false;
 
-    bool debug = false; // toggle debug mode
-    
-    // Obscure internal mapping table
-    unordered_map<char, tuple<int,int,int,int>> mapping = {
-        {'!', {3,0,1,0}},    // (cell delta, nose flip, shadow delta, ghost delta)
-        {'@', {-7,1,2,1}},
-        {'#', {5,0,-1,3}},
-        {'$', {0,0,0,0}},    // output
-        {'~', {0,1,0,0}},
-        {'%', {1,0,3,-2}},
-        {'^', {-3,0,0,5}},
-        {'&', {2,1,-2,0}},
-        {'*', {4,0,1,1}},
-        {'(', {-2,1,0,-1}},
-        {')', {0,0,4,0}},
-        {'_', {0,0,0,1}},
-        {'+', {7,0,-3,2}}
-        // more can be added to increase complexity
+    auto wrap = [](int x) {
+        return (x % 256 + 256) % 256;
     };
 
-    // Randomize code positions internally for extra human-impossibility
-    vector<int> idxs(code.size());
-    iota(idxs.begin(), idxs.end(), 0);
-    // leave original order for now; generator will randomize
+    for (char cmd : code) {
+        int dC = 0, dS = 0, dG = 0, flip = 0;
 
-    for(size_t i=0; i<code.size(); i++){
-        char cmd = code[i];
-        if(mapping.find(cmd) == mapping.end()) continue;
-        auto [dC, flip, dS, dG] = mapping[cmd];
-        
-        cell = (cell + dC + 256) % 256;
-        shadow = (shadow + dS + 256) % 256;
-        ghost = (ghost + dG + 256) % 256;
-        if(flip) nose = 1 - nose;
-        
-        if(cmd == '$'){
-            unsigned char out = (cell + shadow + ghost) % 256;
+        switch (cmd) {
+            case '!': dC=3;  dS=1;  break;
+            case '@': dC=-7; flip=1; dS=2;  dG=1; break;
+            case '#': dC=5;  dS=-1; dG=3; break;
+            case '$': break;
+            case '~': flip=1; break;
+            case '%': dC=1;  dS=3;  dG=-2; break;
+            case '^': dC=-3; dG=5; break;
+            case '&': dC=2;  flip=1; dS=-2; break;
+            case '*': dC=4;  dS=1;  dG=1; break;
+            case '(': dC=-2; flip=1; dG=-1; break;
+            case ')': dS=4; break;
+            case '_': dG=1; break;
+            case '+': dC=7;  dS=-3; dG=2; break;
+            default: continue;
+        }
+
+        cell   = wrap(cell + dC);
+        shadow = wrap(shadow + dS);
+        ghost  = wrap(ghost + dG);
+        if (flip) nose ^= 1;
+
+        if (cmd == '$') {
+            unsigned char out = (cell + shadow + ghost) & 0xFF;
             cout << out;
         }
 
-        if(debug){
-            cout << "\n[DEBUG] cmd=" << cmd 
-                 << " cell=" << cell 
+        if (debug) {
+            cout << "\n[DEBUG] cmd=" << cmd
+                 << " cell=" << cell
                  << " shadow=" << shadow
                  << " ghost=" << ghost
                  << " nose=" << nose << "\n";
         }
     }
 
-    // Extra pseudo-obfuscation loop to confuse humans
-    for(int i=0; i<ghost; i++){
-        cell = (cell + i*i + shadow - ghost) % 256;
-        shadow = (shadow + i*2 - nose) % 256;
-        ghost = (ghost + i*3 + nose) % 256;
+    for (int i = 0; i < ghost; i++) {
+        cell   = wrap(cell + i*i + shadow - ghost);
+        shadow = wrap(shadow + i*2 - nose);
+        ghost  = wrap(ghost + i*3 + nose);
     }
 
-    // Wrap up with random checksum (ignored by interpreter)
-    int checksum = (cell + shadow + ghost + nose) % 256;
+    volatile int checksum = (cell + shadow + ghost + nose) & 0xFF;
     (void)checksum;
 
     return 0;
 }
-
-
-
-
